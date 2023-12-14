9a07838347cdffe32181e096aeabcd31b7107ee3
Pablo Henrique <pablo.h.s.maia@gmail.com>
2023-12-14 11:41:57 -0300

    Main

---
+++ src/main/java/com/generation/stargames/StargamesApplication.java
@@ -0,0 +1,13 @@
+package com.generation.stargames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StargamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StargamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/stargames/StargamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.stargames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StargamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
